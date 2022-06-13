e641089555e62040f0068821904cc4e76ca8cf84
SSK137 <saurabhkodam@gmail.com>
2022-06-14 00:02:47 +0530

    Added class and constructor to create AddressBook Contact

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,23 @@
 console.log("Welcome to Address Book System Using Java Script");
+class AddressBook{
+    constructor(firstName, lastName, address,city,state,zip,phoneNumber,email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    get firstName(){ return this._firstName; }
+    set firstName(firstName){ this._firstName = firstName; }
+
+    toString(){
+        return "\nfirstName : "+this.firstName+"\nlastName : "+this.lastName+"\naddress : "+this.address+
+        "\ncity : "+this.city+"\nstate : "+this.state+"\nzip-code : "+this.zip+"\nphoneNumber : "+this.phoneNumber+"\nEmail-Id : "+this.email;
+    }
+}
+let addressBook = new AddressBook("Saurabh","Kodam","Savedi","Ahmednagar","Maharashtra",414003,8329294765,"saurabhkodam@gmail.com");
+console.log(addressBook.toString());
